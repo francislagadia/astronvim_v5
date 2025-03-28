335b1d12bfc298deaa419bd6716b2c5ef1355dc8
Francis Lagadia <francis.lagadia@emburse.com>
2025-03-27 22:38:25 -0700

    Follow vimdocs casing for keys

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -66,12 +66,14 @@ return {
 
         -- setting a mapping to false will disable it
         -- ["<C-S>"] = false,
-        ["<leader>G"] = { name = "GitHub Copilot" },
-        ["<leader>GG"] = {
+
+        -- add mapping for GitHub Copilot commands
+        ["<Leader>G"] = { name = "GitHub Copilot" },
+        ["<Leader>GG"] = {
           "<cmd>lua require('copilot.panel').open({ position='right', ratio=0.4 })<CR>",
           desc = "Open Copilot Panel in split right",
         },
-        ["<leader>Gr"] = { "<cmd>lua require('copilot.panel').refresh()<CR>", desc = "Refresh suggestions" },
+        ["<Leader>Gr"] = { "<cmd>lua require('copilot.panel').refresh()<CR>", desc = "Refresh suggestions" },
 
         -- vim-easy-align mappings
         ["ga"] = { ":EasyAlign ", desc = "EasyAlign" },
