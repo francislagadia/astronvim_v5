b5a11ff8fc0f899de0dab36b2e955b89e78dd137
Francis Lagadia <francis.lagadia@emburse.com>
2025-03-28 15:53:11 -0700

    Add json, toml, yaml packs

--- lua/community.lua
+++ lua/community.lua
@@ -7,8 +7,11 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.syntax.vim-easy-align" },
   -- import/override with your plugins folder
