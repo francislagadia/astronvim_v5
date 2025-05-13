e62b66597bf0a9738f4accba306131cfbcb354eb
Francis Lagadia <francis.lagadia@emburse.com>
2025-05-12 22:46:22 -0700

    Add solarized-osaka colorscheme

---
+++ lua/plugins/solarized-osaka.lua
@@ -0,0 +1,21 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+return {
+  {
+    "AstroNvim/astroui",
+    ---@type AstroUIOpts
+    opts = {
+      colorscheme = "solarized-osaka",
+    },
+  },
+  {
+    "craftzdog/solarized-osaka.nvim",
+    init = function() -- init function runs before the plugin is loaded
+      vim.o.background = "dark" -- dark or light
+    end,
+    opts = function(_, opts)
+      -- override default options thru the opts table
+      opts.transparent = true
+      opts.terminal_colors = true
+    end,
+  },
+}
