24087d75815fca1a707779386398c7ce76ee1fb0
Francis Lagadia <francis.lagadia@emburse.com>
2025-05-12 22:45:18 -0700

    Add more colorschemes

--- lua/plugins/solarized.lua
+++ lua/plugins/solarized.lua
@@ -4,26 +4,19 @@ return {
     "AstroNvim/astroui",
     ---@type AstroUIOpts
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "solarized",
     },
   },
   {
-    "ellisonleao/gruvbox.nvim",
+    "maxmx03/solarized.nvim",
     init = function() -- init function runs before the plugin is loaded
       vim.o.background = "dark" -- dark or light
+      vim.o.termguicolors = true
     end,
     opts = function(_, opts)
       -- override default options thru the opts table
-      opts.terminal_colors = true
-      opts.contrast = "hard"
-      opts.transparent_mode = true
-      opts.italic = {
-        strings = false,
-        emphasis = false,
-        comments = true,
-        operators = false,
-        folds = true,
-      }
+      -- opts.terminal_colors = true
+      -- opts.transparent = true
     end,
   },
 }
