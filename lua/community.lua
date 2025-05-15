f78503a23d55d7e6c4ce3c5911a15ecf47ff650f
Francis Lagadia <francis.lagadia@emburse.com>
2025-05-14 17:43:17 -0700

    Use gruvbox

--- lua/community.lua
+++ lua/community.lua
@@ -9,7 +9,6 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.pack.python-ruff" },
