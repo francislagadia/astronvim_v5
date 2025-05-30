fbd73fd4565050fd14892f7aa81b945e55b8b27b
Francis Lagadia <francis.lagadia@emburse.com>
2025-05-30 15:34:02 -0700

    disable yaml format on save

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -23,9 +23,11 @@ return {
         enabled = true, -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
           -- "go",
+          "python",
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
           -- "python",
+          "yaml",
         },
       },
       disabled = { -- disable formatting capabilities for the listed language servers
