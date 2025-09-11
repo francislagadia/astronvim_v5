2a84629f822ada9dc2dd839a10bd52a01bc12c36
Francis Lagadia <francis.lagadia@emburse.com>
2025-09-11 10:32:39 -0700

    Add undotree

--- lua/community.lua
+++ lua/community.lua
@@ -11,6 +11,7 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.editing-support.undotree" },
   { import = "astrocommunity.pack.python-ruff" },
   { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.yaml" },
