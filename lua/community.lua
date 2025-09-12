e906f8a691273d60a43b15a2ab0d6a74c159bd30
Francis Lagadia <francis.lagadia@emburse.com>
2025-09-12 10:32:44 -0700

    Add copilot chat

--- lua/community.lua
+++ lua/community.lua
@@ -7,11 +7,12 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.json" },
-  { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.editing-support.copilotchat-nvim" },
   { import = "astrocommunity.editing-support.undotree" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.python-ruff" },
   { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.yaml" },
