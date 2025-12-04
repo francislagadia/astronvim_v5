e49aaa9f7ca4b51feed8b2f5b0cde463991b08a8
Francis Lagadia <francis.lagadia@emburse.com>
2025-12-04 15:15:09 -0800

    Add nvim-spider because it has luarocks and conform to format rest-nvim responses

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,12 @@
+---@type LazySpec
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        json = { "jq" },
+        html = { "prettier" },
+      },
+    },
+  },
+}
